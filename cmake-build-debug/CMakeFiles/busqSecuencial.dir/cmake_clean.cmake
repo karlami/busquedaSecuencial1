f689e8a59a970aa9dcd4884f34567baa278975c2
karlami <karla.rivera24@gmail.com>
2018-08-30 16:26:37 -0600

    Primer commit

---
+++ cmake-build-debug/CMakeFiles/busqSecuencial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/busqSecuencial.dir/main.cpp.o"
+  "busqSecuencial.pdb"
+  "busqSecuencial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/busqSecuencial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
