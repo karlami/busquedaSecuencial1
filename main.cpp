e7d71b1e935c3f99f25588b8374867cabe350412
karlami <karla.rivera24@gmail.com>
2018-08-30 19:24:15 -0600

    busqueda secuencial listo

--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int busquedaSecuencial(int arr[], int num){
+int busquedaSecuencial(int arr[], int num, int size){
     int *p;
-    int size = (sizeof(arr)/ sizeof(int));
+    bool band = false;
     for(int i=0; i < size; i++){
-        p = &arr[0];
+        p = &arr[i];
         if(num == *p){
-            cout << "El numero de carne: "<< *p<< " ha sido encontrado"<< endl;
-        }else{
-            p += 1;
+            band = true;
+            cout << "El numero de carne: "<< *p<< " ha sido encontrado en la posicion: "<< i << endl;
         }
+    }
+    if(!band){
+        cout << "El numero de carne no ha sido encontrado"<< endl;
     }
 
 }
 
 int main() {
-    int listaCarnet[] = {24, 35, 59, 66, 71, 88, 97};
-    cout << "Ingrese el numero de carne que desea buscar de la lista" << endl;
+    int listaCarnet[7] = {2, 35, 59, 2, 71, 88, 97};
+    cout << "Ingrese el numero de carne que desea buscar de la lista: " << endl;
+    int n = (sizeof(listaCarnet)/ sizeof(int));
     int num1;
     cin >> num1;
-    busquedaSecuencial(listaCarnet, num1);
-    cout << num1 << endl;
+    busquedaSecuencial(listaCarnet, num1, n);
     return 0;
 }
